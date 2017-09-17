73ae0bc0573bd4448ed1678b30a07edd0ed5e354
dongcp <dongcp.work@gmail.com>
2017-09-17 23:51:09 +0700

    init project

---
+++ app/src/main/java/self/yue/tvdemo/own/HomeActivity.java
@@ -0,0 +1,19 @@
+package self.yue.tvdemo.own;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import self.yue.tvdemo.R;
+
+/**
+ * Created by dong on 17/09/2017.
+ */
+
+public class HomeActivity extends Activity {
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.home_activity);
+  }
+}
---
+++ app/src/main/java/self/yue/tvdemo/own/HomeFragment.java
@@ -0,0 +1,12 @@
+package self.yue.tvdemo.own;
+
+import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+
+/**
+ * Created by dong on 17/09/2017.
+ */
+
+public class HomeFragment extends BrowseFragment {
+  private ArrayObjectAdapter mRowsAdapter;
+}
